a55b471ec64b0adf617cb43812f59504e8d1637b
github username <github email address>
2018-10-09 16:57:48 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,9 @@
 def count_elements(array)
   # code goes here
+  pet_hash = Hash.new(0)
+  array.each do |x|
+      pet_hash[x] += 1
+  end
+  pet_hash
 end
- 
+count_elements(["dog","cat","fish","fish"])
